fa65edccb23d58cfae77257acb9400cd72cfd33f
Adilet <orozaliev.adilet.07@gmail.com>
2025-04-23 23:45:19 +0600

    fuel

--- bfs/dijkstra_algorithm.py
+++ bfs/dijkstra_algorithm.py
@@ -43,3 +43,44 @@ matrix = [
 
 result = dijkstra(matrix, s - 1, f - 1)
 print(result)
+
+
+
+# Fuel costs and roads
+# В стране N городов, некоторые из которых соединены между собой дорогами. Для того, чтобы проехать по одной дороге требуется один бак бензина. В каждом городе бак бензина имеет разную стоимость. Вам требуется добраться из первого города в N-ый, потратив как можно меньшее количество денег.
+
+def min_cost_to_reach(n, fuel_costs, roads):
+   graph = [[] for _ in range(n)]
+   for u, v in roads:
+      graph[u - 1].append(v - 1)
+      graph[v - 1].append(u - 1)
+
+   dist = [float('inf')] * n
+   dist[0] = 0
+   pq = [(0, 0)]
+
+   while pq:
+      curr_cost, u = heapq.heappop(pq)
+
+      if curr_cost > dist[u]:
+         continue
+
+      for v in graph[u]:
+         new_cost = curr_cost + fuel_costs[u]
+         if new_cost < dist[v]:
+            dist[v] = new_cost
+            heapq.heappush(pq, (new_cost, v))
+
+   return -1 if dist[n - 1] == float('inf') else dist[n - 1]
+
+n = 5
+fuel_costs = [5, 1, 2, 4, 3]
+roads = [
+   (1, 2),
+   (2, 3),
+   (3, 4),
+   (4, 5)
+]
+
+result = min_cost_to_reach(n, fuel_costs, roads)
+print(result)
